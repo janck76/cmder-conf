5819df16a5a886e1f0a5dc37a7c642f3679fee6a
Jan C Kaldestad <janck76@gmail.com>
2016-07-13 23:19:34 +0200

    div

--- setup_profile.cmd
+++ setup_profile.cmd
@@ -6,6 +6,5 @@ echo %CMDER_ROOT% >.cmder_root
 mklink .bashrc %CMDER_ROOT%\config\.bashrc
 mklink .vimrc %CMDER_ROOT%\config\.vimrc
 mklink .minttyrc %CMDER_ROOT%\config\.minttyrc
-mklink .minttycolorscheme_sol.dark %CMDER_ROOT%\config\.minttycolorscheme_sol.dark
 mklink /d .vim %CMDER_ROOT%\config\.vim
 mklink /d .bash-git-prompt %CMDER_ROOT%\config\.bash-git-prompt
